ddb43e2fd4dfb26cc07d4c32746e8eb955ec4495
Patrick Dahlin <patrick.dahlin@gmail.com>
2019-09-27 15:51:52 +0300

    Fixed alignment of error pointer in line causing the error

--- error.lua
+++ error.lua
@@ -1,4 +1,4 @@
-
+require("util")
 local module = {}
 
 
@@ -7,9 +7,18 @@ module.err = function(node, msg)
 		print("------------------------")
 		print(tostring(node.file or "Unknown")..":"..tostring(node.line_nr or 0).." - Syntax error ")
 		print(tostring(node.line_txt or "[Source not avaliable]"))
-		for i=1, node.line_pos or 0, 1 do io.write("-") end
-		print("^")
-		print(tostring(msg or ""))
+        local line = ""
+        -- move caret to error location in text
+        -- We need to add tabs in the correct place
+        -- because of formattnig and how tabs can have different sizes..sigh..
+        for i=1, node.line_pos-1, 1 do
+            local c = string.sub(node.line_txt, i,i)
+            if c == "\t" then line = line .. "\t"
+            else line = line .. " " end
+        end
+        io.write(line)
+        print("^")
+        print(tostring(msg or ""))
 		--print("At symbol: "..tostring(node.type)..","..tostring(node.content or node.op or node.name))
 	else
 		print("Unknown syntax error! "..tostring(msg or ""))
--- util.lua
+++ util.lua
@@ -2,3 +2,15 @@ local match = string.match
 function trim(s)
    return match(s,'^()%s*$') and '' or match(s,'^%s*(.*%S)')
 end
+
+function count_tabs(s, lim)
+   local count = 0
+   if lim == nil or lim > #s then lim = #s end
+   for i = 1, lim do
+      local c = string.sub(s,i,i)
+      if string.byte(c) == 9 then
+         count = count + 1
+      end
+   end
+   return count
+end
